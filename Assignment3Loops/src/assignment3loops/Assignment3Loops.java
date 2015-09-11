3c5e243a3587ea5bf638489d6692604a952054af
stevenPlas <steven.h.plas@gmail.com>
2015-09-11 11:24:22 -0400

    working
    Do-while fix

--- Assignment3Loops/src/assignment3loops/Assignment3Loops.java
+++ Assignment3Loops/src/assignment3loops/Assignment3Loops.java
@@ -5,24 +5,29 @@ public class Assignment3Loops {
     public static void main(String[] args) {
         
         Scanner keyboard = new Scanner(System.in);
-        
-        int cont = 1;
+        int cont;
         
         print("Welcome to Steve's word Mixer-upper!\n\n");
        do{
         print("Please Enter A word you would "
-                + "Like to mix up: ");
-                String word = keyboard.nextLine();
+                + "Like to mix up: ");        
+        String word = keyboard.nextLine();
+                print("\nYour word reversed is: ");
                 for(int i = word.length()-1; i >= 0; i--){
                     System.out.print(word.charAt(i));  
                 }
-        print("\nIf you would like to continue, press 1, if not, press 0 ");
+                
+        print("\n\nIf you would like to continue press 1, if not press 0: ");
         cont = keyboard.nextInt();
+        //to clear the buffer.
+        keyboard.nextLine();
+        print("\n");
+       
        }
-       while(cont != 0);
+       while(cont > 0);
     }
-    
-    public static void print(String str){//print method
+    //print method
+    public static void print(String str){
      System.out.print(str);
  } 
 }
